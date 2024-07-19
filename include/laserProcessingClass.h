0cc917c3074f5a2b39cb4a556444d201536ac7cf
Eugene Voronov <eugene@hmt.ai>
2024-07-19 14:57:47 +0300

    Updated src

--- include/laserProcessingClass.h
+++ include/laserProcessingClass.h
@@ -36,7 +36,7 @@ class LaserProcessingClass
     	LaserProcessingClass();
 		void init(lidar::Lidar lidar_param_in);
 		void featureExtraction(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_in, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_out_edge, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_out_surf);
-		void featureExtractionFromSector(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_in, std::vector<Double2d>& cloudCurvature, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_out_edge, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_out_surf);	
+		void featureExtractionFromSector(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_in, std::vector<Double2d>& cloudCurvature, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_out_edge, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& pc_out_surf, double min_distance);	
 	private:
      	lidar::Lidar lidar_param;
 };
